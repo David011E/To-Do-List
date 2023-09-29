71af4361e1479cc777e3fe5b9be8ab48bc3acfbd
David011e <davidoliver011e@gmail.com>
2023-09-29 12:29:35 +0100

    Add sweetalert for delete button

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -75,12 +75,33 @@ function actions(list_el) {
                 task_input_el.setAttribute("readonly", "readonly");
             }
         } else if (event.target.classList.contains('delete')) {
-            let task_el = event.target.closest('.task');
+
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+              }).then((result) => {
+                
+                if (result.isConfirmed) {
+
+                    let task_el = event.target.closest('.task');
             
             // Check if the task_el is a direct child of list_el before removing it
             if (list_el.contains(task_el)) {
                 list_el.removeChild(task_el);
             }
+                  Swal.fire(
+                    'Deleted!',
+                    'Your task has been deleted.',
+                    'success'
+                  )
+                }
+              })
+            
         }
     });    
 }
